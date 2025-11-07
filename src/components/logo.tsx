03b4ce12ca133555b5091914a0cfefc119f272d3
Leonardo Gomez <leonardo@gomez.cr>
2025-11-07 01:01:57 +0000

    solo que es www.pep.cr y el logo es el que vamos a subir y solamente se

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,10 +4,10 @@ export function Logo() {
   return (
     <div className="flex h-16 items-center justify-center p-2">
       <Image
-        src="https://www.dsf.cr/wp-content/uploads/2021/08/logo-dsf.png"
-        alt="DSF Logo"
+        src="/logopep.png"
+        alt="Credipep Logo"
         width={140}
-        height={40}
+        height={45}
         priority
       />
     </div>
